437d6c6a8c568600d303da3363f7ce147642f107
Gerald Burke <gerald.burke@nerdery.com>
2024-07-15 10:46:01 -0500

    Initial Commit

---
+++ Diver/DiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiverApp.swift
+//  Diver
+//
+//  Created by Gerald Burke on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiverUITests/DiverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DiverUITestsLaunchTests.swift
+//  DiverUITests
+//
+//  Created by Gerald Burke on 7/15/24.
+//
+
+import XCTest
+
+final class DiverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
