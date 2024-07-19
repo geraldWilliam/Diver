1719e31322eaec6ac7eb86291d900ebf7420878f
Gerald Burke <gerald.burke@nerdery.com>
2024-07-19 13:39:50 -0500

    add note on environment usage

--- Diver/DiverApp.swift
+++ Diver/DiverApp.swift
@@ -15,6 +15,10 @@ import TootSDK
 /// - Create an AppDelegate if needed.
 ///
 /// Any user interface setup should happen in `ContentView`.
+///
+/// A note on Environment: Use the environment to inject dependencies that are needed more than one step into the parent-child view hierarchy. The Timeline
+/// presented in ContentView‘s body needs a `PostsController`. Rather than passing the controller to ContentView and then forwarding it to the Timeline, we
+/// put it in the environment.
 // TODO: AppDelegate
 @main struct DiverApp: App {
     var body: some Scene {
