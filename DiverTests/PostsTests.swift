d2bebae5b9ddaa8af9dc2d90ff625f56d98321b7
Gerald Burke <gerald.burke@nerdery.com>
2024-07-24 12:39:30 -0500

    add tests for posts observables

--- DiverTests/PostsTests.swift
+++ DiverTests/PostsTests.swift
@@ -6,30 +6,19 @@
 //
 
 import XCTest
+@testable import Diver
 
-final class PostsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class PostsTests: DiverTests {
+    @MainActor func testItGetsFollowedAuthors() async throws {
+        let subject = Posts(repo: MockPostsRepository())
+        let itHasExpectedPostsCount = expectation(description: #function)
+        withObservationTracking {
+            let _ = subject.timeline
+        } onChange: {
+            itHasExpectedPostsCount.fulfill()
         }
+        subject.getPosts()
+        await fulfillment(of: [itHasExpectedPostsCount], timeout: 1)
+        XCTAssertEqual(12, subject.timeline.count)
     }
-
 }
