85b423a8a6766e0d945f794a071ca3e1dcb5e109
Gerald Burke <gerald.burke@nerdery.com>
2024-07-25 12:25:08 -0500

    remove default comment in ui test

--- DiverUITests/DiverUITests.swift
+++ DiverUITests/DiverUITests.swift
@@ -10,11 +10,7 @@ import XCTest
 final class DiverUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        /// In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        /// In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good
-        /// place to do this.
         XCUIDevice.shared.orientation = .portrait
     }
 
@@ -27,11 +23,6 @@ final class DiverUITests: XCTestCase {
         let app = XCUIApplication()
         app.launchArguments = ["ui-testing"]
         app.launch()
-
-        let homeNavigationBar = XCUIApplication().navigationBars["Home"]
-        homeNavigationBar/*@START_MENU_TOKEN@*/.buttons["Compose"]/*[[".otherElements[\"Compose\"].buttons[\"Compose\"]",".buttons[\"Compose\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        homeNavigationBar.staticTexts["Home"].tap()
-
         /// Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
