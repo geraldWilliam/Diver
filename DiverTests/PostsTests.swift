f99fb3ba39bce441c4308493c479c93d05a8a526
Gerald Burke <gerald.burke@nerdery.com>
2024-07-30 13:01:24 -0500

    complete test coverage for Posts

--- DiverTests/PostsTests.swift
+++ DiverTests/PostsTests.swift
@@ -10,7 +10,8 @@ import XCTest
 
 final class PostsTests: DiverTests {
     @MainActor func testItGetsTimeline() async throws {
-        let subject = Posts(repo: MockPostsRepository())
+        let repo = MockPostsRepository()
+        let subject = Posts(repo: repo)
         let itHasExpectedPostsCount = expectation(description: #function)
         withObservationTracking {
             _ = subject.timeline
@@ -20,5 +21,92 @@ final class PostsTests: DiverTests {
         subject.getLatestPosts()
         await fulfillment(of: [itHasExpectedPostsCount], timeout: 1)
         XCTAssertEqual(12, subject.timeline.count)
+    }
+    
+    @MainActor func testItGetsEarlierPosts() async throws {
+        let repo = MockPostsRepository()
+        let subject = Posts(repo: repo)
+        // Manually set the timeline to the latest posts.
+        subject.timeline = try await repo.getLatestPosts()
+        let itHasExpectedPostsCount = expectation(description: #function)
+        withObservationTracking {
+            _ = subject.timeline
+        } onChange: {
+            itHasExpectedPostsCount.fulfill()
+        }
+        subject.getEarlierPosts()
+        await fulfillment(of: [itHasExpectedPostsCount], timeout: 1)
+        XCTAssertEqual(24, subject.timeline.count)
+    }
+    
+    @MainActor func testItGetsRepliesForPost() async throws {
+        let repo = MockPostsRepository()
+        let subject = Posts(repo: repo)
+        let itHasRepliesForPost = expectation(description: #function)
+        withObservationTracking {
+            _ = subject.replies
+        } onChange: {
+            itHasRepliesForPost.fulfill()
+        }
+        let post = PostInfo.mock()
+        subject.getReplies(for: post)
+        await fulfillment(of: [itHasRepliesForPost])
+        XCTAssertEqual(12, subject.replies[post.id]?.count)
+    }
+    
+    @MainActor func testGetTimelineRaisesErrorOnFailure() async throws {
+        let repo = FailingMockPostsRepository()
+        let subject = Posts(repo: repo)
+        let itRaisesError = expectation(description: #function)
+        withObservationTracking {
+            _ = subject.failure
+        } onChange: {
+            itRaisesError.fulfill()
+        }
+        subject.getLatestPosts()
+        await fulfillment(of: [itRaisesError])
+        XCTAssertNotNil(subject.failure)
+    }
+    
+    @MainActor func testGetEarlierPostsRaisesErrorOnFailure() async throws {
+        let repo = FailingMockPostsRepository()
+        let subject = Posts(repo: repo)
+        let itRaisesError = expectation(description: #function)
+        withObservationTracking {
+            _ = subject.failure
+        } onChange: {
+            itRaisesError.fulfill()
+        }
+        subject.getEarlierPosts()
+        await fulfillment(of: [itRaisesError])
+        XCTAssertNotNil(subject.failure)
+    }
+    
+    @MainActor func testGetRepliesForPostRaisesErrorOnFailure() async throws {
+        let repo = FailingMockPostsRepository()
+        let subject = Posts(repo: repo)
+        let itRaisesError = expectation(description: #function)
+        withObservationTracking {
+            _ = subject.failure
+        } onChange: {
+            itRaisesError.fulfill()
+        }
+        subject.getReplies(for: .mock())
+        await fulfillment(of: [itRaisesError])
+        XCTAssertNotNil(subject.failure)
+    }
+}
+
+private struct FailingMockPostsRepository: PostsRepositoryProtocol {
+    func getLatestPosts() async throws -> [Diver.PostInfo] {
+        throw Failure(#function)
+    }
+    
+    func getEarlierPosts() async throws -> [Diver.PostInfo] {
+        throw Failure(#function)
+    }
+    
+    func getReplies(for post: Diver.PostInfo) async throws -> [Diver.PostInfo] {
+        throw Failure(#function)
     }
 }
