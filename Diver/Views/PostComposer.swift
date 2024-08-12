b3d37317274ac8b110174c16914fd4bb3363a8b3
Gerald Burke <gerald.burke@nerdery.com>
2024-08-12 09:15:53 -0500

    more post composer view work and a small change to terminology in readme

--- Diver/Views/PostComposer.swift
+++ Diver/Views/PostComposer.swift
@@ -10,60 +10,89 @@ import SwiftUI
 @MainActor struct PostComposer: View {
     @Environment(Navigator.self) var navigator
     @Environment(Posts.self) var posts
+    
     @State private var textContent: String = ""
+
+    @State private var attachedMedia: [(URL, UUID)] = [
+        (URL(string: "https://picsum.photos/100/100")!, UUID()),
+        (URL(string: "https://picsum.photos/100/100")!, UUID()),
+        (URL(string: "https://picsum.photos/100/100")!, UUID()),
+    ]
+
     @FocusState private var isFocused: Bool
     
     var body: some View {
         NavigationStack {
-            VStack {
-                TextField("Write a post", text: $textContent, axis: .vertical)
-                    .padding()
-                    .lineLimit(8...)
-                    .focused($isFocused)
-                    .overlay {
-                        RoundedRectangle(cornerSize: CGSize(width: 12, height: 12))
-                            .stroke(Color.gray, lineWidth: 1.0)
-                    }
-                    .padding()
-                   
-                Spacer()
-
-                HStack {
-                    Button(action: { }) {
-                        Image(systemName: "number")
-                            .frame(maxWidth: .infinity)
-                    }
-                    Button(action: { }) {
-                        Image(systemName: "photo")
-                            .frame(maxWidth: .infinity)
-                    }
-                    Button(action: { }) {
-                        Image(systemName: "at")
-                            .frame(maxWidth: .infinity)
-                    }
-                    Button(action: { }) {
-                        Image(systemName: "exclamationmark.triangle")
-                            .frame(maxWidth: .infinity)
-                    }
-                }
-                .primaryButtonStyle()
-                .frame(maxWidth: .infinity)
-                .padding()
-            }
-            .onAppear {
-                isFocused = true
-            }
-            .navigationTitle("Compose")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button(action: { cancelPost() }) {
-                        Text("Cancel")
+            ScrollView {
+                VStack {
+                    TextField("Write a post", text: $textContent, axis: .vertical)
+                        .padding()
+                        .lineLimit(8...)
+                        .focused($isFocused)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.gray, lineWidth: 1.0)
+                        }
+                        .padding(.horizontal)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(attachedMedia, id: \.1) { item in
+                                AsyncImage(url: item.0)
+                                    .clipShape(
+                                        RoundedRectangle(cornerRadius: 10)
+                                    )
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.white)
+                                    }
+                                    
+                            }
+                        }
+                        .padding()
+                        .shadow(radius: 2)
                     }
                 }
-                ToolbarItem {
-                    Button(action: { sendPost() }) {
-                        Text("Send")
+                .onAppear {
+                    isFocused = true
+                }
+                .navigationTitle("Compose")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button(action: { cancelPost() }) {
+                            Text("Cancel")
+                        }
+                    }
+                    ToolbarItem {
+                        Button(action: { sendPost() }) {
+                            Text("Send")
+                        }
+                    }
+                    ToolbarItemGroup(placement: .keyboard) {
+                        HStack {
+                            Button(action: { }) {
+                                Image(systemName: "number")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                            }
+                            Button(action: { }) {
+                                Image(systemName: "photo")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                            }
+                            Button(action: { }) {
+                                Image(systemName: "at")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                            }
+                            Button(action: { }) {
+                                Image(systemName: "exclamationmark.triangle")
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
                     }
                 }
             }
