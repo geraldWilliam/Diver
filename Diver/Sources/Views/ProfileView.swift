399c657ac4749a5a42bed3069c1563f8de9fd806
Gerald Burke <gerald@tagleaf.com>
2024-12-14 21:05:37 -0600

    tweaks to ProfileView, exploring options for showing followers, posts, etc.

--- Diver/Sources/Views/ProfileView.swift
+++ Diver/Sources/Views/ProfileView.swift
@@ -11,71 +11,96 @@ struct ProfileView: View {
     @Environment(Accounts.self) var accounts
     @Environment(Session.self) var session
     let account: AccountInfo
-
+    @State private var selectedContentMode: Int = 0
     var body: some View {
         ScrollView {
             LazyVStack {
-                AsyncImage(url: account.headerImage) { image in
-                    image
-                        .resizable()
-                } placeholder: {
-                    Image(systemName: "exclamationmark.circle")
-                }
-                .frame(maxWidth: .infinity)
-                .frame(height: 200)
-                HStack {
-                    AsyncImage(url: account.profileImage) { image in
+                ZStack {
+                    AsyncImage(url: account.headerImage) { image in
                         image
                             .resizable()
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .frame(maxWidth: .infinity)
                     } placeholder: {
                         Image(systemName: "exclamationmark.circle")
                     }
-                    .aspectRatio(1, contentMode: .fit)
-                    .frame(width: 70, height: 70)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 200)
+                    .overlay {
+                        Rectangle()
+                            .fill(
+                                LinearGradient(
+                                    colors: [.black.opacity(0.8), .clear],
+                                    startPoint: .bottom,
+                                    endPoint: .top
+                                )
+                            )
+                    }
+                 
+                    // Nested stack to get bottom-left alignment for pfp.
+                    VStack {
+                        Spacer()
+                        HStack {
+                            AsyncImage(url: account.profileImage) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            } placeholder: {
+                                EmptyView()
+                            }
+                            .frame(width: 70, height: 70)
+                            
+                            Spacer()
+                            
+                            VStack(alignment: .leading) {
+                                if accounts.following.contains(account) {
+                                    Button(action: { accounts.unfollow(account.id) }) {
+                                        Text("Unfollow")
+                                            .foregroundStyle(Color.white)
+                                    }
+                                } else if account != session.currentSession?.account {
+                                    Button(action: { accounts.follow(account.id) }) {
+                                        Text("Follow")
+                                            .foregroundStyle(Color.white)
+                                    }
+                                }
+                            }
+                            .buttonStyle(BorderedButtonStyle())
+                        }
+                    }
                     .padding()
+                }
 
-                    Divider()
-
+                HStack {
                     VStack(alignment: .leading) {
                         Text(account.displayName)
                             .font(.headline)
                         Text(account.handle)
                             .foregroundStyle(Color.secondary)
                     }
+                    Spacer()
                 }
-
-                VStack(alignment: .leading) {
-                    if accounts.following.contains(account) {
-                        Button(action: { accounts.unfollow(account.id) }) {
-                            Text("Unfollow")
-                        }
-                    } else if account != session.currentSession?.account {
-                        Button(action: { accounts.follow(account.id) }) {
-                            Text("Follow")
-                        }
-                    }
+                .frame(maxWidth: .infinity)
+                .multilineTextAlignment(.leading)
+                .padding(.vertical)
+                
+                // TODO: Extract above into separate view. AccountView? AuthorView?
+                
+                Picker(selection: $selectedContentMode) {
+                    Text("Followers: \(account.followersCount) ")
+                        .tag(0)
+                    Text("Following: \(account.followingCount)")
+                        .tag(1)
+                    Text("Posts: \(account.postCount)")
+                        .tag(2)
+                } label: {
+                    EmptyView()
                 }
-                .buttonStyle(BorderedButtonStyle())
-
-                VStack {
-                    HStack {
-                        Button(action: {}) {
-                            Text("\(account.followersCount) Followers")
-                        }
-                        Spacer()
-                    }
-                    HStack {
-                        Button(action: {}) {
-                            Text("Following \(account.followingCount)")
-                        }
-                        Spacer()
-                    }
-                    HStack {
-                        Button(action: {}) {
-                            Text("\(account.postCount) Posts")
-                        }
-                        Spacer()
+                .pickerStyle(.segmented)
+                
+                List {
+                    ForEach(0..<account.followersCount, id: \.self) { index in
+                        
                     }
                 }
             }
@@ -85,9 +110,7 @@ struct ProfileView: View {
 }
 
 #Preview {
-    List {
-        ProfileView(account: .mock())
-    }
-    .environment(Accounts(repo: MockAccountRepository()))
-    .environment(Session(repo: MockSessionRepository()))
+    ProfileView(account: .mock())
+        .environment(Accounts(repo: MockAccountRepository()))
+        .environment(Session(repo: MockSessionRepository()))
 }
